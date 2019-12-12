810bb96b2030c9aec5399bb87745e6324bac2cbb
Richard Lee <richlee81192@gmail.com>
2019-12-12 03:29:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,14 +3,3 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 end
 
-get '/name' do
-  "My name is Richard!"
-end
-
-get '/hometown' do
-  "My hometown is Burbank"
-end
-
-get 'favorite_song' do
-  "My favorite song is I dont know."
-end
